23b87c0cac1b9cbbe60ce97e2c677a7bf0f79625
Jalil Laaraichi <reachjalil@gmail.com>
2023-04-26 14:13:02 -0600

    nexter par

---
+++ app/part/[param]/page.tsx
@@ -0,0 +1,9 @@
+export const runtime = "edge";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      param
+    </main>
+  );
+}
